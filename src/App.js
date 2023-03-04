1e019e78a284ac1ef8d37ed567e659f83fe661a0
maciek367 <maciek2baba2@gmail.com>
2023-03-04 23:38:08 +0100

    change css, add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <p>Hello There</p>
-  );
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
