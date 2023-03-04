a05339f74d05e53b69bfcbbc3a6f3fc19f98e6a7
maciek367 <maciek2baba2@gmail.com>
2023-03-04 23:41:27 +0100

    fix: Route has to be wrapped in Routes component

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Route path="/*" element={<App />} />
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
