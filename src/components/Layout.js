5e356c439faaef53c4e2f37d8d3fb9f354250a39
maciek367 <maciek2baba2@gmail.com>
2023-03-06 21:03:32 +0100

    change layout - nav is inside header now

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header"
+import Nav from "./Nav";
+import Footer from "./Footer"
+
 
 const Layout = () => {
-  return <Outlet />
-}
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
