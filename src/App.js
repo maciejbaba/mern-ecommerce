2c5e47872019e1bb70b15a8a083f531ac5eb9b8f
maciek367 <maciek2baba2@gmail.com>
2023-03-09 21:44:29 +0100

    add item page comp

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
 import Login from "./components/Login";
-import Cart from "./components/Cart"
+import Cart from "./components/Cart";
+import ItemPage from "./components/ItemPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Public />} /> 
+        <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
         <Route path="cart" element={<Cart />} />
+        <Route path=":id" element={<ItemPage />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/ItemPage.js
@@ -0,0 +1,7 @@
+import { useParams } from "react-router-dom";
+
+const ItemPage = () => {
+  return <main>ItemPage</main>;
+};
+
+export default ItemPage;
