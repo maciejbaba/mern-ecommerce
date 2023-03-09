bed3ca649e05068cf9ad81f62721ff3c7e9fa118
maciek367 <maciek2baba2@gmail.com>
2023-03-09 21:45:21 +0100

    add public comp css

--- src/components/Public.js
+++ src/components/Public.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import "../css/Public.css";
 
 const Public = () => {
-  return <main>Public page</main>;
+  return <main className="public">Public page</main>;
 };
 
 export default Public;
