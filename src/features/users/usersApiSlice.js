83b5be7782cce1ddab2b17e3f8a4bccef8e1d711
maciek367 <maciek2baba2@gmail.com>
2023-03-18 17:45:38 +0100

    add usersApiSlice

---
+++ src/features/users/usersApiSlice.js
@@ -0,0 +1,90 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../../app/api/apiSlice";
+
+const usersAdapter = createEntityAdapter({});
+
+const initialState = usersAdapter.getInitialState();
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => ({
+        url: "/users",
+        method: "GET",
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError;
+        },
+      }),
+      transformResponse: (responseData) => {
+        const loadedUsers = responseData.map((user) => {
+          user.id = user._id;
+          return user;
+        });
+        return usersAdapter.setAll(initialState, loadedUsers);
+      },
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: "User", id: "LIST" },
+            ...result.ids.map((id) => ({ type: "User", id })),
+          ];
+        }
+      },
+    }),
+    addNewUser: builder.mutation({
+      query: (newUserData) => ({
+        url: "/users",
+        method: "POST",
+        body: {
+          ...newUserData,
+        },
+      }),
+      invalidatesTags: [{ type: "User", id: "LIST" }],
+    }),
+    updateUser: builder.mutation({
+      query: (newUserData) => ({
+        url: "/users",
+        method: "PATCH",
+        body: {
+          ...newUserData,
+        },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "User", id: arg.id }],
+    }),
+    deleteUser: builder.mutation({
+      query: ({ id }) => ({
+        url: "/users",
+        method: "DELETE",
+        body: { id },
+      }),
+      invalidatesTags: (result, error, arg) => [
+        {
+          type: "User",
+          id: arg.id,
+        },
+      ],
+    }),
+  }),
+});
+
+export const {
+  useGetUsersQuery,
+  useAddNewUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+} = usersApiSlice;
+
+export const selectUsers = usersApiSlice.endpoints.getUsers.select();
+
+const selectUsersData = createSelector(
+  selectUsers,
+  (usersResult) => usersResult.data
+);
+
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+  selectIds: selectUsersIds,
+} = usersAdapter.getSelectors(
+  (state) => selectUsersData(state) ?? initialState
+);
