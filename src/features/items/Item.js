27d1b88deec40cd24fa3c854245db3dfaa0f56c6
maciek367 <maciek2baba2@gmail.com>
2023-03-18 21:23:35 +0100

    add Item component

---
+++ src/features/items/Item.js
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux"
+import { selectItemById } from "./itemsApiSlice"
+
+const Item = ({ id }) => {
+  const item = useSelector(state => selectItemById(state, id))
+
+  return (
+    <div>
+      <img src={item.photoURL} alt={item.photoURL === "no-image.png" ? "Image didn't load" : `Photo of ${item.name}`} />
+    </div>
+  )
+}
+
+export default Item
