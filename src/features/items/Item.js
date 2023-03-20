7edbe7d31da6de3d6ff94edd174759a58bcee906
maciek367 <maciek2baba2@gmail.com>
2023-03-20 21:37:28 +0100

    apply prettier

--- src/features/items/Item.js
+++ src/features/items/Item.js
@@ -1,14 +1,21 @@
-import { useSelector } from "react-redux"
-import { selectItemById } from "./itemsApiSlice"
+import { useSelector } from "react-redux";
+import { selectItemById } from "./itemsApiSlice";
 
 const Item = ({ id }) => {
-  const item = useSelector(state => selectItemById(state, id))
+  const item = useSelector((state) => selectItemById(state, id));
 
   return (
     <div>
-      <img src={item.photoURL} alt={item.photoURL === "no-image.png" ? "Image didn't load" : `Photo of ${item.name}`} />
+      <img
+        src={item.photoURL}
+        alt={
+          item.photoURL === "no-image.png"
+            ? "Image didn't load"
+            : `Photo of ${item.name}`
+        }
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
