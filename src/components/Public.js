6a489a4058f5c25bf0b9b04885f757130f4b4ecb
maciek367 <maciek2baba2@gmail.com>
2023-03-20 21:37:01 +0100

    apply prettier, change layout

--- src/components/Public.js
+++ src/components/Public.js
@@ -7,15 +7,19 @@ const Public = () => {
   const itemsListRef = useRef();
 
   const handleBrowseItemsButtonClick = () => {
-    itemsListRef.current?.scrollIntoView({behavior: 'smooth'});
+    itemsListRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <main className="public">
-      <div className="content">
+      <div className="hero">
         <h1>Welcome to our store!</h1>
         <p>We hope that you will find something for yourself</p>
-        <button className="browse-items" onClick={handleBrowseItemsButtonClick}>Browse Items</button>
+        <button className="browse-items-button" onClick={handleBrowseItemsButtonClick}>
+          Browse Items
+        </button>
+      </div>
+      <div>
         <ItemsList ref={itemsListRef} />
       </div>
     </main>
