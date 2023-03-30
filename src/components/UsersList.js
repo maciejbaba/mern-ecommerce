b502dc5a7246affd7a9859f340c8dafcca3f12f8
maciek367 <maciek2baba2@gmail.com>
2023-03-30 22:03:13 +0200

    add basic UsersList component and its css

---
+++ src/components/UsersList.js
@@ -0,0 +1,24 @@
+import "../css/UsersList.css"
+
+const UsersList = () => {
+  return (
+    <main>
+      <div>
+        <h1>Other users</h1>
+      </div>
+      <div className="users-list">
+        <p>user 1</p>
+        <p>user 2</p>
+        <p>user 3</p>
+        <p>user 4</p>
+        <p>user 5</p>
+        <p>user 6</p>
+        <p>user 7</p>
+        <p>user 8</p>
+        <p>user 9</p>
+      </div>
+    </main>
+  )
+}
+
+export default UsersList
