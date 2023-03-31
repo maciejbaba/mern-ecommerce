a189658302aa9101f8263df2f94945a95aa3b41a
maciek367 <maciek2baba2@gmail.com>
2023-03-31 23:11:47 +0200

    add basic login form and css

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -1,6 +1,18 @@
+import "../../css/Login.css"
+
 const Login = () => {
   return (
-    <main>Login page</main>
+    <main className="main-login">
+      <div>
+        <h1>Login</h1>
+      </div>
+      <form action="" className="login-form">
+        <label htmlFor="username">Username: </label>
+        <input type="text" />
+        <label htmlFor="password">Password: </label>
+        <input type="password" />
+      </form>
+    </main>
   )
 }
 
