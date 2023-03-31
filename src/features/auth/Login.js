baa5f9c344dd9b0bbba4f5f440ba0404c8335e57
maciek367 <maciek2baba2@gmail.com>
2023-03-31 23:30:38 +0200

    add input controlling, change styling, add state

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -1,19 +1,36 @@
-import "../../css/Login.css"
+import "../../css/Login.css";
+import { useState } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <main className="main-login">
       <div>
-        <h1>Login</h1>
+        <h1>Welcome!</h1>
       </div>
       <form action="" className="login-form">
-        <label htmlFor="username">Username: </label>
-        <input type="text" />
-        <label htmlFor="password">Password: </label>
-        <input type="password" />
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.currentTarget.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
+        <button className="login-btn" onClick={(e) => e.preventDefault()}>
+          Login
+        </button>
       </form>
     </main>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
