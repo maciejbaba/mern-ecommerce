26a1587680c5f012f1d1b28183bf8512b547319b
maciek367 <maciek2baba2@gmail.com>
2023-04-01 12:09:46 +0200

    add showing user data and add formating date logic

--- src/features/users/User.js
+++ src/features/users/User.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { selectUserById } from "./usersApiSlice";
 import { useSelector } from "react-redux";
-import "../../css/User.css"
+import "../../css/User.css";
+
+const changeDateFormat = (mongoDBDate) => {
+  // example, returns "18/03/2023" from "2023-03-18T20:06:37.926Z"
+  const ddmmyyyyDate = new Date(mongoDBDate).toLocaleDateString();
+  return ddmmyyyyDate;
+};
 
 const User = ({ id }) => {
   const user = useSelector((state) => selectUserById(state, id));
 
   return (
-    <div>
-      <p>{user.username}</p>
+    <div className="user">
+      <p>Username: {user.username}</p>
+      <p>Status: {user.active ? "active" : "unactive"}</p>
+      <p>Roles: {user.roles}</p>
+      <p>Created at: {changeDateFormat(user.createdAt)}</p>
+      <p>Last update: {changeDateFormat(user.updatedAt)}</p>
     </div>
   );
 };
