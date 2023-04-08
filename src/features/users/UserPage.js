e929fb6523e4b22394f3a8e1ecebee018b71c727
maciek367 <maciek2baba2@gmail.com>
2023-04-08 18:47:32 +0200

    add userPage comp

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,5 +1,16 @@
+import User from "./User";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUserById } from "./usersApiSlice";
+
 const UserPage = () => {
-  return <div>UserPage</div>;
+  const { id } = useParams();
+  const user = useSelector((state) => selectUserById(state, id));
+
+  return <main style={{textAlign: "center"}}>
+    <h1>User {user.username}</h1>
+    <User id={id} />
+  </main>;
 };
 
 export default UserPage;
