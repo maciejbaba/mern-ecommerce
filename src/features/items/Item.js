cf87ce3fb0b5c885cc99dc0a9a11832d4103b0bf
maciek367 <maciek2baba2@gmail.com>
2023-04-09 23:58:01 +0200

    fix: description overflowed when too long

--- src/features/items/Item.js
+++ src/features/items/Item.js
@@ -10,14 +10,14 @@ const Item = ({ id }) => {
     <div className="item-container">
       <img
         className="item-img"
-        src={item.photoURL}
+        src={item.photoURL ? item.photoURL : "no-image.png"}
         alt={
           item.photoURL === "no-image.png"
             ? "Image didn't load"
             : `Photo of ${item.name}`
         }
       />
-      <h2 className="item-name">{item.name}</h2>
+      <h2 className="item-name">{item.name}</h2> {/* change magic number */}
       <p className="item-description">{item.description.length < 150 ? item.description : item.description.slice(0, 150) + "..."}</p>
       <p className="item-price">Price: {item.price}</p>
       <button className="add-to-cart-button" onClick={handleAddToCart}>
