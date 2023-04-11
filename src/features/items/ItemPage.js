cc1f4a7623ff2f8acb6ea76f8238a42a5785954e
maciek367 <maciek2baba2@gmail.com>
2023-04-11 22:58:00 +0200

    add ItemPage

--- src/features/items/ItemPage.js
+++ src/features/items/ItemPage.js
@@ -1,7 +1,33 @@
 import { useParams } from "react-router-dom";
+import { useGetItemsQuery } from "./itemsApiSlice";
+import "../../css/ItemPage.css";
 
 const ItemPage = () => {
-  return <main>ItemPage</main>;
+  const { id } = useParams();
+  const { data: items, isLoading, isSuccess } = useGetItemsQuery();
+  const item = items?.entities[id];
+
+  const handleAddToCart = () => {};
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (isSuccess) {
+    content = (
+      <main className="item-page-main">
+        <h1>Item {item?.name}</h1>
+        <img src={item?.photoURL} alt={item?.name} />
+        <p>Description {item?.description}</p>
+        <p>Price: {item?.price}</p>
+        <button className="item-page-button-add" onClick={handleAddToCart}>
+          Add to cart
+        </button>
+      </main>
+    );
+  }
+
+  return content;
 };
 
 export default ItemPage;
