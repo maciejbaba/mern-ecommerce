5d08b6c5d7cac04d16b18045936e708e4a9b4510
maciek367 <maciek2baba2@gmail.com>
2023-04-11 22:58:21 +0200

    make items link to themselves

--- src/features/items/Item.js
+++ src/features/items/Item.js
@@ -1,29 +1,36 @@
 import { useSelector } from "react-redux";
 import { selectItemById } from "./itemsApiSlice";
 import "../../css/Item.css";
+import { Link } from "react-router-dom";
 
 const Item = ({ id }) => {
   const item = useSelector(state => selectItemById(state, id));
 
   const handleAddToCart = () => {};
   return (
-    <div className="item-container">
-      <img
-        className="item-img"
-        src={item.photoURL ? item.photoURL : "no-image.png"}
-        alt={
-          item.photoURL === "no-image.png"
-            ? "Image didn't load"
-            : `Photo of ${item.name}`
-        }
-      />
-      <h2 className="item-name">{item.name}</h2> {/* change magic number */}
-      <p className="item-description">{item.description.length < 150 ? item.description : item.description.slice(0, 150) + "..."}</p>
-      <p className="item-price">Price: {item.price}</p>
-      <button className="add-to-cart-button" onClick={handleAddToCart}>
-        Add to cart
-      </button>
-    </div>
+    <Link to={`/items/${item.id}`}>
+      <div className="item-container">
+        <img
+          className="item-img"
+          src={item.photoURL ? item.photoURL : "no-image.png"}
+          alt={
+            item.photoURL === "no-image.png"
+              ? "Image didn't load"
+              : `Photo of ${item.name}`
+          }
+        />
+        <h2 className="item-name">{item.name}</h2> {/* change magic number */}
+        <p className="item-description">
+          {item.description.length < 150
+            ? item.description
+            : item.description.slice(0, 150) + "..."}
+        </p>
+        <p className="item-price">Price: {item.price}</p>
+        <button className="add-to-cart-button" onClick={handleAddToCart}>
+          Add to cart
+        </button>
+      </div>
+    </Link>
   );
 };
 
