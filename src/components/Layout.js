abc1eedb2713a876baa886a4aa10462fd8a194a0
maciek367 <maciek2baba2@gmail.com>
2023-04-12 18:31:19 +0200

    add ScrollToTop after route change

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 
 const Layout = () => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       {/* very hacky */}
       <div style={{ marginTop: "5rem" }}>
