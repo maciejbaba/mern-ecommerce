3608ce41b6bc422d49f18aaeff448a7ef36d757c
maciek367 <maciek2baba2@gmail.com>
2023-04-14 23:15:42 +0200

    add saving to localStorage

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,13 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+
+const saveItemsToLocalStorage = items => {
+  localStorage.setItem("cartItems", JSON.stringify(items));
+};
+
+const getItemsFromLocalStorage = () => {
+  const items = localStorage.getItem("cartItems");
+  return items ? JSON.parse(items) : [];
+};
+
+const removeItemsFromLocalStorage = () => {
+  localStorage.removeItem("cartItems");
+};
 
 const initialState = {
-  items: [],
+  items: getItemsFromLocalStorage(),
   total: 0,
   quantity: 0,
 };
 
 export const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
   reducers: {
     addToCart: (state, action) => {
@@ -15,15 +28,18 @@ export const cartSlice = createSlice({
       state.items = [...state.items, item];
       state.quantity = state.items.length;
       state.total = state.items.reduce((acc, item) => acc + item.price, 0);
+      saveItemsToLocalStorage(state.items);
     },
     removeFromCart: (state, action) => {
       const item = action.payload;
       state.items = state.items.filter(cartItem => cartItem.id !== item);
       state.quantity = state.items.length;
       state.total = state.items.reduce((acc, item) => acc + item.price, 0);
+      saveItemsToLocalStorage(state.items);
     },
-    emptyCart: (state) => {
+    emptyCart: state => {
       state = initialState;
+      removeItemsFromLocalStorage();
     },
   },
 });
