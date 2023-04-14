b73fc24edcbc726ee3c36efd684a847a9a5faf42
maciek367 <maciek2baba2@gmail.com>
2023-04-14 23:39:20 +0200

    add remove item logic to Cart, change styling

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,21 +1,30 @@
 import "../../css/Cart.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromCart } from "./cartSlice";
 
 const Cart = () => {
   const items = useSelector(state => state.cart.items);
+
+  const dispatch = useDispatch();
+
+  const handleRemoveItem = (item) => {
+    dispatch(removeFromCart(item))
+  };
 
   return (
     <main className="main-cart">
       <div className="cart-heading">
         <h1>Your items</h1>
+        <button>Remove all items</button>
       </div>
       <div className="user-items-list">
         {items.map(item => (
           <div className="user-item" key={item.id}>
-            <img src={item.photoURL} />
+            <img className="cart-img" src={item.photoURL} />
             <h2>{item.name}</h2>
             <p>{item.description.length > 50 ? item.description.slice(0, 50) + "..." : item.description}</p>
             <p>{item.price}</p>
+            <button onClick={() => handleRemoveItem(item)}>Remove</button>
           </div>
         ))}
       </div>
