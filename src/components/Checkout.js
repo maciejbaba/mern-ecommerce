9263c45adf136d69a121090a9208210ce5a981ae
maciek367 <maciek2baba2@gmail.com>
2023-04-17 18:16:33 +0200

    add basic Checkout component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,8 +1,12 @@
+import "../css/Checkout.css"
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
-  )
-}
+    <main className="main-checkout">
+      <h1>Checkout</h1>
+      <p>Here should be the checkout form, but it's not real store :D</p>
+    </main>
+  );
+};
 
-export default Checkout
+export default Checkout;
