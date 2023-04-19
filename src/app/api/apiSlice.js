ba04ce3f564e580dfd318020603413aa259940b0
maciek367 <maciek2baba2@gmail.com>
2023-04-19 22:01:04 +0200

    change baseUrl

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-ecommerce-api-pt6x.onrender.com" }),
   tagTypes: ["Item", "User"],
   endpoints: (builder) => ({}),
 });
