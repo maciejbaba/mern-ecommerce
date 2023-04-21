ce0419059a5d065bb3f54e5547fa7ac446d2343b
maciek367 <maciek2baba2@gmail.com>
2023-04-21 17:30:40 +0200

    add different backend links in dev and prod

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+let baseUrl = "https://mern-ecommerce-api-pt6x.onrender.com";
+
+if (window.location.origin.includes("localhost")) {
+  baseUrl = "http://localhost:3500";
+}
+
+
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-ecommerce-api-pt6x.onrender.com" }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   tagTypes: ["Item", "User"],
   endpoints: (builder) => ({}),
 });
