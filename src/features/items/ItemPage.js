58a091d0392f32ae370c56a6395d12f4965a03a8
maciek367 <maciek2baba2@gmail.com>
2023-04-24 20:05:53 +0200

    refactor ItemPage

--- src/features/items/ItemPage.js
+++ src/features/items/ItemPage.js
@@ -6,29 +6,44 @@ import { addToCart } from "../cart/cartSlice";
 
 const ItemPage = () => {
   const { id } = useParams();
-  const { data: items, isLoading, isSuccess } = useGetItemsQuery();
-  const item = items?.entities[id];
+  const { data: items, isLoading, isSuccess, isError } = useGetItemsQuery();
 
   const dispatch = useDispatch();
-
-  const handleAddToCart = () => {
-    dispatch(addToCart(item));
-  };
 
   let content;
 
   if (isLoading) {
-    content = <p>Loading...</p>;
-  } else if (isSuccess) {
+    content = (
+      <main className="item-page__loading">
+        <p>Loading item data...</p>
+      </main>
+    );
+  }
+
+  if (isSuccess) {
+    const handleAddToCart = () => dispatch(addToCart(item));
+    const item = items.entities[id];
+
     content = (
       <main className="item-page">
-        <h1>Item {item?.name}</h1>
-        <img className="item-page__img" src={item?.photoURL} alt={item?.name} />
-        <p>Description {item?.description}</p>
-        <p>Price: {`${item?.price} $`}</p>
-        <button className="item-page__add-to-cart-button" onClick={handleAddToCart}>
+        <h1>Item {item.name}</h1>
+        <img className="item-page__img" src={item.photoURL} alt={item.name} />
+        <p>Description {item.description}</p>
+        <p>Price: {`${item.price} $`}</p>
+        <button
+          className="item-page__add-to-cart-button"
+          onClick={handleAddToCart}
+        >
           Add to cart
         </button>
+      </main>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <main className="item-page__error">
+        <p>Sorry we cannot load item's data</p>
       </main>
     );
   }
