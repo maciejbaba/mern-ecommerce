53d6ed51de5fce58eb3a2e07ebf51875cb4d33f6
maciek367 <maciek2baba2@gmail.com>
2023-05-08 20:02:33 +0200

    add comment to tsconfig, change build folder to build for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   test: {
     globals: true,
