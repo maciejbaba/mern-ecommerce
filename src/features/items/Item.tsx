82667b63637c2eee13b103bb5b8ad9921356be08
maciek367 <maciek2baba2@gmail.com>
2023-05-13 00:12:55 +0200

    type Item

--- src/features/items/Item.tsx
+++ src/features/items/Item.tsx
@@ -3,52 +3,76 @@ import { selectItemById } from "./itemsApiSlice";
 import "../../css/Item.css";
 import { addToCart } from "../cart/cartSlice";
 import { Link } from "react-router-dom";
+import { RootState } from "../../app/store";
+import { EntityId } from "@reduxjs/toolkit";
+import MyButton from "../../components/myButton";
 
 const DEFAULT_PHOTO_URL = "./no-image.png";
 const MAX_DESCRIPTION_LENGTH = 50;
 
-const Item = ({ id }) => {
-  const item = useSelector((state) => selectItemById(state, id));
+type ItemProps = {
+  id: EntityId;
+};
 
+const Item = ({ id }: ItemProps) => {
+  const item = useSelector((state: RootState) => selectItemById(state, id));
   const dispatch = useDispatch();
 
-  const handleAddToCart = (e) => {
-    e.preventDefault();
-    dispatch(addToCart(item));
-  };
+  let content;
 
-  return (
-    <Link to={`/items/item/${item.id}`}>
-      <main className="item">
-        <img
-          className="item__img"
-          src={
-            item.photoURL === DEFAULT_PHOTO_URL
-              ? DEFAULT_PHOTO_URL
-              : item.photoURL
-          }
-          alt={
-            item.photoURL === DEFAULT_PHOTO_URL
-              ? "Image didn't load"
-              : `Photo of ${item.name}`
-          }
-        />
-        <h2 className="item__name">{item.name}</h2> {/* change magic number */}
-        <p className="item__description">
-          {item.description.length < MAX_DESCRIPTION_LENGTH
-            ? item.description
-            : item.description.slice(0, MAX_DESCRIPTION_LENGTH) + "..."}
+  if (!item) {
+    content = (
+      <div className="item__error">
+        <p>
+          Opss the item cannot be loaded. We are truly sorry, item was not found
         </p>
-        <p className="item__price">Price: {`${item.price} $`}</p>
-        <MyButton
-          className="item__add-to-cart-button"
-          onClick={handleAddToCart}
-        >
-          Add to cart
-        </MyButton>
-      </main>
-    </Link>
-  );
+      </div>
+    );
+  } else {
+
+    const handleAddToCart = (
+      e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      e.preventDefault();
+      dispatch(addToCart(item)); // function call is inside else block to prevent dispatching undefined
+    };
+
+    content = (
+      <Link to={`/items/item/${item.id}`}>
+        <main className="item">
+          <img
+            className="item__img"
+            src={
+              item.photoURL === DEFAULT_PHOTO_URL
+                ? DEFAULT_PHOTO_URL
+                : item.photoURL
+            }
+            alt={
+              item.photoURL === DEFAULT_PHOTO_URL
+                ? "Image didn't load"
+                : `Photo of ${item.name}`
+            }
+          />
+          <h2 className="item__name">{item.name}</h2>{" "}
+          {/* change magic number */}
+          <p className="item__description">
+            {item.description.length < MAX_DESCRIPTION_LENGTH
+              ? item.description
+              : item.description.slice(0, MAX_DESCRIPTION_LENGTH) + "..."}
+          </p>
+          <p className="item__price">Price: {`${item.price} $`}</p>
+          <MyButton
+            className="item__add-to-cart-button"
+            onClick={handleAddToCart}
+          >
+            Add to cart
+          </MyButton>
+        </main>
+      </Link>
+    );
+
+    return content;
+  }
 };
 
 export default Item;
