6717d3d5dcc7dce1fea55ddf0eb6ffb7e3514e86
maciek367 <maciek2baba2@gmail.com>
2023-05-12 23:06:43 +0200

    add RootState type and AppDispatch type to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,9 +2,6 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import cartSliceReducer from "../features/cart/cartSlice";
 
-const rootReducer = combineReducers({});
-export type RootState = ReturnType<typeof rootReducer>;
-
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
@@ -14,3 +11,6 @@ export const store = configureStore({
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
