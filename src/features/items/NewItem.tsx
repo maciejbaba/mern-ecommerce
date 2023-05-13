aa145ebdc674e0a5c0fccf190c5541b13bbff6df
maciek367 <maciek2baba2@gmail.com>
2023-05-13 18:53:13 +0200

    type NewItem

--- src/features/items/NewItem.tsx
+++ src/features/items/NewItem.tsx
@@ -1,22 +1,23 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useAddNewItemMutation } from "./itemsApiSlice";
 import "../../css/NewItem.css";
+import MyButton from "../../components/myButton";
 
 const NewItem = () => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState(0);
-  const [photoURL, setPhotoURL] = useState("");
+  const [name, setName] = useState<string>();
+  const [description, setDescription] = useState<string>();
+  const [price, setPrice] = useState<number>();
+  const [photoURL, setPhotoURL] = useState<string>();
 
   const [addNewItem, { isSuccess, isLoading, isError }] =
     useAddNewItemMutation();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     addNewItem({ name, description, price, photoURL });
   };
 
-  const handleReset = (e) => {
+  const handleReset = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setName("");
     setDescription("");
@@ -27,7 +28,7 @@ const NewItem = () => {
   return (
     <main className="new-item">
       <h1>Add new item</h1>
-      <form onSubmit={handleSubmit} className="new-item__form">
+      <form className="new-item__form">
         <div className="new-item__input-container">
           <label htmlFor="name">Name</label>
           <input
@@ -40,7 +41,6 @@ const NewItem = () => {
         <div className="new-item__input-container">
           <label htmlFor="description">Description</label>
           <textarea
-            type="text"
             id="description"
             value={description}
             placeholder="Enter a description..."
@@ -55,7 +55,7 @@ const NewItem = () => {
             type="number"
             id="price"
             value={price}
-            onChange={(e) => setPrice(e.target.value)}
+            onChange={(e) => setPrice(Number(e.target.value))}
           />
         </div>
         <div className="new-item__input-container">
