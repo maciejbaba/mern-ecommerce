b09864773651833b8d7ae1e243cdc9378d8bcdee
maciek367 <maciek2baba2@gmail.com>
2023-05-13 18:57:36 +0200

    type EditUser

--- src/features/users/EditUser.tsx
+++ src/features/users/EditUser.tsx
@@ -8,17 +8,28 @@ const EditUser = () => {
 
   // fetch user, using selector resulted in an error due to attempting to access user.username before user was defined
   const { data: users } = useGetUsersQuery();
-  const user = users?.entities[id];
 
   let content;
 
-  // todo - add error handling, loading state from api instead of using this
-  if (!user) {
-    content = <p>Loading user data...</p>;
+  if (!id) {
+    content = <p>Invalid user id</p>;
   }
 
-  if (user) {
-    content = <EditUserForm user={user} />;
+  if (!users) {
+    content = <p>No users found</p>;
+  }
+
+  if (users && users.entities && id) {
+    const user = users.entities[id];
+
+    // todo - add error handling, loading state from api instead of using this
+    if (!user) {
+      content = <p>Loading user data...</p>;
+    }
+
+    if (user) {
+      content = <EditUserForm user={user} />;
+    }
   }
 
   return <main className="edit-user">{content}</main>;
