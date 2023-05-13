381af282fe5f1817f2d647918b56cfe2f84a2aa6
maciek367 <maciek2baba2@gmail.com>
2023-05-13 21:53:19 +0200

    type User

--- src/features/users/User.tsx
+++ src/features/users/User.tsx
@@ -2,6 +2,8 @@ import { selectUserById, useDeleteUserMutation } from "./usersApiSlice";
 import { useSelector } from "react-redux";
 import "../../css/User.css";
 import { useNavigate } from "react-router-dom";
+import { RootState } from "../../app/store";
+import MyButton from "../../components/myButton";
 
 export const changeDateFormat = (mongoDBDate: string): string => {
   // example, returns "18/03/2023" from "2023-03-18T20:06:37.926Z"
@@ -15,18 +17,19 @@ export const changeDateFormat = (mongoDBDate: string): string => {
   return ddmmyyyyDate;
 };
 
-const User = ({ id }) => {
-  const user = useSelector((state) => selectUserById(state, id));
+type UserProps = {
+  id: string;
+};
+
+const User = ({ id }: UserProps) => {
+  const user = useSelector((state: RootState) => selectUserById(state, id));
   const navigate = useNavigate();
 
   const [deleteUser, { isLoading, isError, error }] = useDeleteUserMutation();
 
-  const handleEditUser = (e) => {
-    e.preventDefault();
-    navigate(`/users/editUser/${user.id}`);
-  };
-
-  const handleDeleteUser = (e) => {
+  const handleDeleteUser = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
     deleteUser(user);
     if (isError) return alert(error);
@@ -34,20 +37,35 @@ const User = ({ id }) => {
     alert("User deleted");
   };
 
-  return (
-    <div className="user">
-      <p>Username: {user.username}</p>
-      <p>Status: {user.active ? "Active" : "Unactive"}</p>
-      <p>isAdmin: {user.isAdmin ? "Yes" : "No"}</p>
-      <p>Created at: {changeDateFormat(user.createdAt)}</p>
-      <p>Last update: {changeDateFormat(user.updatedAt)}</p>
-      <div className="user__buttons">
-        <MyButton onClick={handleEditUser}>Edit</MyButton>
-        <MyButton onClick={handleDeleteUser}>Delete</MyButton>
+  let content;
+
+  if (!user) {
+    content = <p>User not found</p>;
+  } else {
+    const handleEditUser = (
+      e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      e.preventDefault();
+      navigate(`/users/editUser/${user.id}`);
+    };
+
+    content = (
+      <div className="user">
+        <p>Username: {user.username}</p>
+        <p>Status: {user.active ? "Active" : "Unactive"}</p>
+        <p>isAdmin: {user.isAdmin ? "Yes" : "No"}</p>
+        <p>Created at: {changeDateFormat(user.createdAt)}</p>
+        <p>Last update: {changeDateFormat(user.updatedAt)}</p>
+        <div className="user__buttons">
+          <MyButton onClick={handleEditUser}>Edit</MyButton>
+          <MyButton onClick={handleDeleteUser}>Delete</MyButton>
+        </div>
+        {isLoading && <p>Deleting user...</p>}
       </div>
-      {isLoading && <p>Deleting user...</p>}
-    </div>
-  );
+    );
+  }
+
+  return content;
 };
 
 export default User;
