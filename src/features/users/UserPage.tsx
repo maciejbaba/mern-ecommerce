e6fbee33decf545cdd8bcfd64be2ceb084aa4a06
maciek367 <maciek2baba2@gmail.com>
2023-05-13 21:55:35 +0200

    type UserPage

--- src/features/users/UserPage.tsx
+++ src/features/users/UserPage.tsx
@@ -6,19 +6,26 @@ import { RootState } from "../../app/store";
 
 const UserPage = () => {
   const { id } = useParams();
-  const user = useSelector((state: RootState) => selectUserById(state, id));
 
   let content;
 
-  if (!user) {
-    content = <p>User not found</p>;
-  } else if (user) {
-    content = (
-      <main style={{ textAlign: "center" }}>
-        <h1>User {user.username}</h1>
-        <User id={id} />
-      </main>
-    );
+  if (!id) {
+    content = <p>Invalid user ID</p>;
+  }
+
+  if (id) {
+    const user = useSelector((state: RootState) => selectUserById(state, id)); // has to be here because of possible undefined id
+
+    if (!user) {
+      content = <p>User not found</p>;
+    } else if (user) {
+      content = (
+        <main style={{ textAlign: "center" }}>
+          <h1>User {user.username}</h1>
+          <User id={id} />
+        </main>
+      );
+    }
   }
 
   return content;
