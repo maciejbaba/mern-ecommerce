963d8636e7fd48ba6558f94eeeecf90969c6131d
maciek367 <maciek2baba2@gmail.com>
2023-05-28 21:02:43 +0200

    add RemoveItemsList

--- src/features/items/RemoveItemsList.tsx
+++ src/features/items/RemoveItemsList.tsx
@@ -1,7 +1,55 @@
+import MyButton from "../../components/myButton";
 import "../../css/RemoveItemsList.css";
+import Item from "./Item";
+import { useGetItemsQuery } from "./itemsApiSlice";
 
 const RemoveItemsList = () => {
-  return <div>RemoveItemsList</div>;
+  const {
+    data: items,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetItemsQuery();
+
+  const handleDeleteItemFromStore = () => {};
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading items...</p>;
+  } else if (isSuccess) {
+    content = (
+      <main className="remove-items__main">
+        <div>
+          <h1>Items</h1>
+        </div>
+        <div className="remove-items__items-list">
+          {items.ids.map((itemId) => (
+            <div className="remove-items__item-div">
+              <Item id={itemId} key={itemId} />
+              <MyButton className="remove-items__remove-item-button" onClick={handleDeleteItemFromStore}>
+                Delete item from store
+              </MyButton>
+            </div>
+          ))}
+        </div>
+      </main>
+    );
+  } else if (isError && "data" in error) {
+    content = (
+      <div>
+        <p>Snap! ERROR</p>
+        <p>
+          {error.status} {JSON.stringify(error)}
+        </p>
+      </div>
+    );
+  } else if (isError) {
+    content = <p>{JSON.stringify(error)}</p>;
+  }
+
+  return content;
 };
 
 export default RemoveItemsList;
