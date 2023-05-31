3ca79d690272a54ebc996c1c065f006cee9bd787
maciek367 <maciek2baba2@gmail.com>
2023-05-31 21:36:56 +0200

    add EditItem component

--- src/features/items/EditItem.tsx
+++ src/features/items/EditItem.tsx
@@ -2,21 +2,21 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { selectItemById } from "./itemsApiSlice";
 import { RootState } from "../../app/store";
+import EditItemForm from "./EditItemForm";
 
 const EditItem = () => {
   const { id } = useParams();
 
-  let content;
-
   if (!id) {
-    content = <p>"Missing item id"</p>;
-    alert("Missing item id");
+    return <p>"Missing item id"</p>;
   }
-  if (id) {
-    const item = useSelector((state: RootState) => selectItemById(state, id));
+  const item = useSelector((state: RootState) => selectItemById(state, id));
+
+  if (!item) {
+    return <p>"Item not found"</p>;
   }
 
-  return <div>EditItem</div>;
+  return <EditItemForm item={item} />;
 };
 
 export default EditItem;
