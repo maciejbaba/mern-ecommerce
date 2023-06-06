25a199e2d13fea31c1db5c2e2e6257c8496f3f3b
maciek367 <maciek2baba2@gmail.com>
2023-06-07 00:24:10 +0200

    set up EditItemForm

--- src/features/items/EditItemForm.tsx
+++ src/features/items/EditItemForm.tsx
@@ -10,9 +10,41 @@ const EditItemForm = ({ item }: EditItemFormProps) => {
   const [name, setName] = useState(item.name);
   const [description, setDescription] = useState(item.description);
   const [price, setPrice] = useState(item.price);
-  const [photoUrl, setPhotoUrl] = useState(item.photoURL);
+  const [photoURL, setPhotoURL] = useState(item.photoURL);
 
-  const [updateItem, { isLoading, isError, error }] = useUpdateItemMutation();
+  const [updateItem, { isSuccess, isLoading, isError, error }] =
+    useUpdateItemMutation();
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleDescriptionChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setDescription(e.target.value);
+  };
+
+  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice(Number(e.target.value));
+  };
+
+  const handlePhotoUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhotoURL(e.target.value);
+  };
+
+  const handleEditItemFormSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    await updateItem({ id: item.id, name, description, price, photoURL });
+    if (isError) {
+      alert("Something went wrong");
+    }
+    if (isSuccess) {
+      alert("Item has been updated successfully");
+    }
+  };
 
   return <div>EditItemForm</div>;
 };
