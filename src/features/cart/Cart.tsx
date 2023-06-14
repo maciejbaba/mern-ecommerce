7896ce916f3c15178da6d925e56e14536aa723eb
maciek367 <maciek2baba2@gmail.com>
2023-06-14 18:02:57 +0200

    implement cart handling quantity logic

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -1,12 +1,19 @@
 import "../../css/Cart.css";
 import { useDispatch, useSelector } from "react-redux";
-import { removeFromCart, emptyCart, selectCartItems } from "./cartSlice";
+import {
+  removeFromCart,
+  emptyCart,
+  selectCartItems,
+  selectCartTotal,
+} from "./cartSlice";
 import { useNavigate } from "react-router-dom";
 import MyButton from "../../components/myButton";
-import { Item } from "../items/itemsApiSlice";
+import { CartItem } from "./cartSlice";
 
 const Cart = (): JSX.Element => {
-  const items: Item[] = useSelector(selectCartItems);
+  const items: CartItem[] = useSelector(selectCartItems);
+
+  const total: number = useSelector(selectCartTotal);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -15,12 +22,11 @@ const Cart = (): JSX.Element => {
     navigate("/cart/checkout");
   };
 
-  const handleRemoveItem = (item: Item) =>
-    dispatch(removeFromCart(item));
+  const handleRemoveItem = (item: CartItem) => dispatch(removeFromCart(item));
 
   const handleRemoveAll = () => dispatch(emptyCart());
 
-  let content = <p>Loading cart...</p>; // basic JSX.Element otherwise TS complains
+  let content = <p>Loading cart...</p>;
 
   if (items.length === 0 || !items) {
     content = <p className="cart__empty-cart">Your cart is empty</p>;
@@ -30,19 +36,10 @@ const Cart = (): JSX.Element => {
     content = (
       <main className="cart">
         <div className="cart__heading">
-          <MyButton
-            className="cart__remove-all-button"
-            onClick={handleRemoveAll}
-          >
-            Remove all items
-          </MyButton>
-          <MyButton className="cart__buy-items-button" onClick={handleBuy}>
-            Buy items
-          </MyButton>
           <h1>Your items</h1>
         </div>
         <div className="cart__user-items-list">
-          {items.map((item: Item) => (
+          {items.map((item: CartItem) => (
             <div className="cart__user-item" key={item.id}>
               <img
                 className="cart__img"
@@ -56,10 +53,23 @@ const Cart = (): JSX.Element => {
                   : item.description}
               </p>
               <p>{item.price}</p>
+              <p>Quantity: {item.quantity}</p>
               <MyButton onClick={() => handleRemoveItem(item)}>Remove</MyButton>
             </div>
           ))}
         </div>
+        <footer className="cart__footer">
+          <MyButton
+            className="cart__remove-all-button"
+            onClick={handleRemoveAll}
+          >
+            Remove all items
+          </MyButton>
+          <p className="cart__footer__total-p">{total}</p>
+          <MyButton className="cart__buy-items-button" onClick={handleBuy}>
+            Buy items
+          </MyButton>
+        </footer>
       </main>
     );
   }
