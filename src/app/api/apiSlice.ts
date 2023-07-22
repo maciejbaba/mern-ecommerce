c1999b744618a7da0f6907bc4ab36002164d76fd
maciek367 <maciek2baba2@gmail.com>
2023-07-22 21:36:24 +0200

    fix server, go back to using it

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -4,6 +4,7 @@ export let baseUrl: string = "https://mern-ecommerce-api-pt6x.onrender.com";
 
 if (window.location.origin.includes("localhost")) {
   baseUrl = "http://192.168.0.14:3500"; // my server ip on local network
+  // baseUrl = "http://localhost:3500"; // dev server
 }
 
 export const apiSlice = createApi({
