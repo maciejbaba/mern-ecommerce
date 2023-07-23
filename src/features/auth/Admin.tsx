bf70d8ac1e7a9b9d06637e49c66388d53f2bdd78
maciek367 <maciek2baba2@gmail.com>
2023-07-23 15:28:06 +0200

    add global classes, style not logged in part of admin component, add rerouting

--- src/features/auth/Admin.tsx
+++ src/features/auth/Admin.tsx
@@ -1,25 +1,38 @@
-import { useSelector } from "react-redux"
-import { selectUser } from "../auth/sessionSlice"
+import { useSelector } from "react-redux";
+import { selectUser } from "../auth/sessionSlice";
+import "../../css/Admin.css";
+import { useNavigate } from "react-router-dom";
+import MyButton from "../../components/myButton";
 
 const Admin = () => {
   const user = useSelector(selectUser);
   console.log(user);
-  
-  
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
   if (!user) {
     return (
-      <div>
-        Not logged in, in order to see the admin panel you have to be logged in
-        and be an admin
+      <div className="admin__not-logged">
+        <p>
+          <span className="text-red">Not logged in</span>, in order to see the
+          admin panel <span className="text-red">you have to be logged in</span>{" "}
+          and be <span className="text-red">an admin</span>
+        </p>
+        <MyButton className="button" onClick={handleLoginClick}>
+          Login
+        </MyButton>
       </div>
     );
   }
 
   if (!user.isAdmin) {
-    return <div>You are not an admin</div>;
+    return <div className="admin__not-admin">You are not an admin</div>;
   }
 
-  return <div>Admin panel</div>;
+  return <div className="admin">Admin panel</div>;
 };
 
 export default Admin;
