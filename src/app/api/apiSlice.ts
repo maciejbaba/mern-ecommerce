a325ee0a654fd2782a1c0b187102176d50188856
maciejbaba <maciek2baba2@gmail.com>
2025-09-25 23:35:46 +0200

    udpate url

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export let baseUrl: string = "http://localhost:3500";
+export let baseUrl: string = import.meta.env.VITE_API_URL || "http://localhost:3500";
 
 export const apiSlice = createApi({
   reducerPath: "api",
